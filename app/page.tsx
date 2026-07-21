799136c55560c9ca81f467b6eb99a6e1a1f7d914
jason-ngn <bennguyen20122006@gmail.com>
2026-07-21 23:44:54 +0700

    repositioned speed metrics to layout file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import Dashboard from "./pages/Dashboard";
 
 export default function Home() {
-	return (
-		<div>
-			<SpeedInsights />
-			<Dashboard />
-		</div>
-	);
+	return <Dashboard />;
 }
